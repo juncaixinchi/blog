6864540a9d7c76b95670a9c8ce60cda0a89fc47b
juncaixinchi <juncaixinchi111@gmail.com>
2017-02-22 22:22:06 +0800

    add new post

--- auto_run.sh
+++ auto_run.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+hexo clean
+hexo g
 cp source/404.html public/404.html
 cp source/404/page.js public/404/page.js
 echo "File Copied !"
+hexo d
---
+++ source/_posts/Javascript-note.md
@@ -0,0 +1,11 @@
+---
+title: Javascript-note
+date: 2017-02-22 21:57:57
+tags: 
+    - javascript
+    - ES6
+---
+
+# 学习javascript的笔记
+
+balabla
